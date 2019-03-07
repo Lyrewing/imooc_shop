d3cc34984007bd0d3636eb6392f9a990a78273d5
e3c_100005333972_iven <e3c_100005333972_iven@git.cloud.tencent.com>
2019-03-07 17:05:08 +0800

    ddd

--- lib/video/video.dart
+++ lib/video/video.dart
@@ -1,49 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
-class VideoPage extends StatefulWidget{
+import 'package:chewie/chewie.dart';
+class VideoPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return null;
+    return VideoPageState();
   }
 }
 
-class VideoPageState extends State<VideoPage>{
+class VideoPageState extends State<VideoPage> {
   VideoPlayerController _controller;
+  ChewieController _chewieController;
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    _controller = VideoPlayerController.network('http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4')..initialize().then((_){
-      setState(() {});
-    });
+    _controller = VideoPlayerController.network('http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4');
+    _chewieController =ChewieController(
+      videoPlayerController: _controller,
+      aspectRatio: 16/9,
+      autoPlay: true,
+      looping: true
+
+    );
   }
-  @override
-  void dispose(){
-    super.dispose();
-    _controller.dispose();
-  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    final Size size = _controller.value.size;
     return Scaffold(
-      body: Center(
-        child: _controller.value.initialized?AspectRatio(
-          aspectRatio: _controller.value.aspectRatio,
-          child: VideoPlayer(_controller),
-        ):Container(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            _controller.value.isPlaying?_controller.pause():_controller.play();
-          });
-        },
-        child: Icon(
-          _controller.value.isPlaying?Icons.pause:Icons.play_arrow
+      body: Container(
+        child: Center(
+          child: Chewie(
+            controller: _chewieController,
+          ),
         ),
       ),
     );
   }
 
-
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+    _chewieController.dispose();
+  }
 }
